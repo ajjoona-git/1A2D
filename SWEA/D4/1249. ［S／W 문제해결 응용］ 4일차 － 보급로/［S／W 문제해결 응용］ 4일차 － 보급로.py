cff9a485cd53c36b566c71fd3a7eac4c7898ef93
ajjoona-git <ajjoona@gmail.com>
2025-09-08 16:39:55 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 215 ms, Memory: 63,488 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,45 +1,39 @@
+# 1249. 보급로
 import heapq
-# import sys
-# sys.stdin = open("input.txt")
+
+# 이동 경로는 상하좌우
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+INF = 1e9  # 충분히 큰 수 (문제에서 도로 깊이의 범위를 안 줌)
+
+def dijkstra(sr, sc):
+    dist = [[INF] * N for _ in range(N)]
+
+    dist[sr][sc] = 0
+    pq = [(0, sr, sc)]
+
+    while pq:
+        curr_d, r, c = heapq.heappop(pq)
+
+        # 종료조건: 도착지에 도착한 경우
+        if (r, c) == (N-1, N-1):
+            return dist[r][c]
+
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
+            # 최소거리 갱신
+            if (
+                0 <= nr < N and 0 <= nc < N
+                and curr_d + grid[nr][nc] < dist[nr][nc]
+            ):
+                dist[nr][nc] = curr_d + grid[nr][nc]
+                heapq.heappush(pq, (dist[nr][nc], nr, nc))
+                
+    
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     grid = [list(map(int, input().strip())) for _ in range(N)]
 
-    S = (0, 0)
-    G = (N-1, N-1)
-
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
-
-    # 해당 좌표까지 최소 복구 시간을 저장할 리스트.
-    dist = [[float('inf')] * N for _ in range(N)]
-    hq = []
-
-    dist[S[0]][S[1]] = 0
-    heapq.heappush(hq, (0, 0, 0))
-
-    while hq:
-        cost, r, c = heapq.heappop(hq)
-
-        # 최소보다 현재 비용이 크다면, 넘어감
-        if cost > dist[r][c]:
-            continue
-
-        # 도착하면 종료
-        if (r, c) == G:
-            result = cost
-            break
-
-        # 인접 4방향 탐색
-        for i in range(4):
-            nr, nc = r + dr[i], c + dc[i]
-            # 경계 체크
-            if 0 <= nr < N and 0 <= nc < N:
-                ncost = cost + grid[nr][nc]
-                if ncost < dist[nr][nc]:
-                    dist[nr][nc] = ncost
-                    heapq.heappush(hq, (ncost, nr, nc))
-
-    print(f'#{tc} {result}')
+    print(f'#{tc} {dijkstra(0, 0)}')
