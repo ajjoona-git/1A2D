c6dc838b291bfd36365cfb62d89e3162d3b317c6
ajjoona-git <ajjoona@gmail.com>
2025-09-12 17:58:38 +0900

    [D4] Title: 정사각형 방, Time: 137 ms, Memory: 65,664 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -4,20 +4,22 @@
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-# def move(r, c, count):
-#     global max_count, start
+def move(r, c):
+    global max_count, start
+    # 종료조건
+    if visited[r][c] > 0:
+        return visited[r][c]
 
-#     for i in range(4):
-#         nr, nc = r + dr[i], c + dc[i]
-#         if 0 <= nr < N and 0 <= nc < N and grid[nr][nc] == grid[r][c] + 1:
-#             print(grid[r][c])
-#             move(nr, nc, count + 1)
-#             break
-#     else:
-#         if count >= max_count:
-#             max_count = count
-#             start = min(start, grid[r][c])
-#             return
+    visited[r][c] = 1
+    for i in range(4):
+        nr, nc = r + dr[i], c + dc[i]
+        if (0 <= nr < N and 0 <= nc < N 
+            and grid[nr][nc] == grid[r][c] + 1
+        ):
+            visited[r][c] += move(nr, nc)
+            break
+
+    return visited[r][c]
     
 
 T = int(input())
@@ -25,36 +27,20 @@ for tc in range(1, T+1):
     N = int(input())
     grid = [list(map(int, input().split())) for _ in range(N)]
 
-    max_count = 0
-    # start = N ** 2
+    visited = [[0] * N for _ in range(N)]
     
-    # 인접한 방에 현재 방번호보다 1 크다면, 리스트에 저장
-    linked = [0] * (N ** 2 + 1)
+    max_count = 0
+    start = 0
 
     for r in range(N):
         for c in range(N):
-            for i in range(4):
-                nr, nc = r + dr[i], c + dc[i]
-                if not (0 <= nr < N and 0 <= nc < N):
-                    continue
-
-                if grid[nr][nc] == grid[r][c] + 1:
-                    linked[grid[r][c]] = 1
-                    continue
-    
-    # 최대로 연속되는 방의 개수 구하기
-    num = 1
-    start = 1
-    while num < N ** 2:
-        # 연속된 1의 수(count)
-        count = 1
-        if linked[num] == 1:
-            while linked[num + count] == 1:
-                count += 1
-                # num += 1
+            count = move(r, c)
+            # 최대값 갱신
             if count > max_count:
                 max_count = count
-                start = num
-        num += count
+                start = grid[r][c]
+            # 시작 방 번호 갱신
+            elif count == max_count and grid[r][c] < start:
+                start = grid[r][c]
 
-    print(f'#{tc} {start} {max_count + 1}')
+    print(f'#{tc} {start} {max_count}')
