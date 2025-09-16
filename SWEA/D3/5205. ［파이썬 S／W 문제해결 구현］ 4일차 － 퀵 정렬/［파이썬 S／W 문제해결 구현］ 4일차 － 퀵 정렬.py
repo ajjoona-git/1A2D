6f28b8d0c479049a56d7a6a6685c91487eb1bfca
ajjoona-git <ajjoona@gmail.com>
2025-09-16 18:28:20 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 4일차 - 퀵 정렬, Time: 1,086 ms, Memory: 159,416 KB -BaekjoonHub

--- SWEA/D3/5205. ［파이썬 S／W 문제해결 구현］ 4일차 － 퀵 정렬/［파이썬 S／W 문제해결 구현］ 4일차 － 퀵 정렬.py
+++ SWEA/D3/5205. ［파이썬 S／W 문제해결 구현］ 4일차 － 퀵 정렬/［파이썬 S／W 문제해결 구현］ 4일차 － 퀵 정렬.py
@@ -1,21 +1,34 @@
 # 5205. 퀵 정렬
 
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
+def partition(arr, start, end):
+    # Lumoto 파티션 방식: 가장 마지막 원소를 피벗으로 설정
+    pivot = arr[end]
+    i = start - 1
 
-    pivot = arr[len(arr) // 2]  # 중앙값을 기준으로
-    middle = [x for x in arr if x == pivot]
-    left = [x for x in arr if x < pivot]
-    right = [x for x in arr if x > pivot]
+    for j in range(start, end):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
 
-    return quick_sort(left) + middle + quick_sort(right)
+    arr[i + 1], arr[end] = arr[end], arr[i + 1]
+
+    return i + 1
+
+
+def quick_sort(arr, start, end):
+    if start < end:
+        # 1. 분할
+        pivot_idx = partition(arr, start, end)
+
+        # 2. 정복
+        quick_sort(arr, start, pivot_idx - 1)
+        quick_sort(arr, pivot_idx + 1, end)
 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = list(map(int, input().split()))
-    sorted_arr = quick_sort(arr)
-    
-    print(f'#{tc}', sorted_arr[N // 2])
+
+    quick_sort(arr, 0, N - 1)
+    print(f"#{tc} {arr[N // 2]}")
